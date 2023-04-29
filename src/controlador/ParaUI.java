726d5cd48a193c0d64d72050db2a6fafb8e2d177
AngelAcedo12 <117735273+AngelAcedo12@users.noreply.github.com>
2023-04-29 12:33:38 +0200

    loader

--- src/controlador/ParaUI.java
+++ src/controlador/ParaUI.java
@@ -30,12 +30,12 @@ public class ParaUI extends UI {
 	protected GestionSonidos gestionSonidos;
 	private Victoria victoriaPanel;
 	private Derrota derrotaPanel;
-	
+
 	public ParaUI(Victoria victoriaPanel, Derrota derrotaPanel) {
 		this.gestionSonidos = new GestionSonidos();
-		this.victoriaPanel =  victoriaPanel;
+		this.victoriaPanel = victoriaPanel;
 		this.derrotaPanel = derrotaPanel;
-		
+
 		behaviourDifficultyButtons(getDifficultyButton());
 		this.miGestion = new GestionTablero(Dificultad.medio);
 		createMouseListener(true);
@@ -47,7 +47,7 @@ public class ParaUI extends UI {
 	private void listenerSliceControl() {
 		JSlider slider = getSlider();
 		slider.addChangeListener(new ChangeListener() {
-			
+
 			public void stateChanged(ChangeEvent e) {
 				float volumenActual = getSlider().getValue();
 				gestionSonidos.cambiarVolumen(volumenActual);
@@ -58,24 +58,25 @@ public class ParaUI extends UI {
 
 	private MouseListener createMouseListener(boolean velada) {
 		MouseListener mouseListener = new MouseListener() {
-			private boolean hover=velada;
-			public void mouseReleased(MouseEvent e) {				
+			private boolean hover = velada;
+
+			public void mouseReleased(MouseEvent e) {
 				Coordenada cordenadasBoton = ((BotonCasilla) e.getSource()).getCoordenada();
-				//CLICK IZQUIERDO
-				if(e.getButton()==1) {
+				// CLICK IZQUIERDO
+				if (e.getButton() == 1) {
 					miGestion.hacerMovimiento(cordenadasBoton);
 					reprodicirEfecto();
 					actualizarTablero();
-					
-					if(miGestion.getNumeroMinas() == miGestion.getMiTablero().getCasillasVeladas()) {
+
+					if (miGestion.getNumeroMinas() == miGestion.getMiTablero().getCasillasVeladas()) {
 						mostrarVictoria();
 						removeAllMouseListener();
 						reproducirEfectoVictoria();
-						
+
 					}
 				}
-				//CLICK DERECHO
-				if(e.getButton()==3) {
+				// CLICK DERECHO
+				if (e.getButton() == 3) {
 					miGestion.marcarCasilla(cordenadasBoton);
 					actualizarTablero();
 				}
@@ -85,65 +86,65 @@ public class ParaUI extends UI {
 			}
 
 			public void mouseExited(MouseEvent e) {
-				if(hover) quitHover((BotonCasilla) e.getSource());
-
+				if (hover)
+					quitHover((BotonCasilla) e.getSource());
 
 			}
 
 			public void mouseEntered(MouseEvent e) {
-				if(hover)setHover((BotonCasilla) e.getSource());
+				if (hover)
+					setHover((BotonCasilla) e.getSource());
 			}
 
 			public void mouseClicked(MouseEvent e) {
 				setClicked((BotonCasilla) e.getSource());
-				
+
 			}
-			
+
 		};
 		return mouseListener;
 	}
 
 	public void actualizarTablero() {
 		TableroAleatorio miTablero = miGestion.getMiTablero();
-		//miTablero.showMinasTablero();
+		// miTablero.showMinasTablero();
 		boolean terminado = false;
 		for (int i = 0; i < miTablero.getAncho() && !terminado; i++) {
 			for (int j = 0; j < miTablero.getAlto() && !terminado; j++) {
 				Coordenada coordenadaActual = new Coordenada(i, j);
 				BotonCasilla boton = getPanelMinador().getBoton(coordenadaActual);
 				// SI NO ESTÁ DESVELADA, IMPRIMIR VACIO
-				if(miTablero.getCasilla(coordenadaActual).isMarcada()) {
+				if (miTablero.getCasilla(coordenadaActual).isMarcada()) {
 
-					
-					Image flagIMG = Toolkit.getDefaultToolkit().getImage("resources/FLAG.png").getScaledInstance(
-							boton.getWidth()-3, boton.getWidth()-3, Image.SCALE_DEFAULT);
+					Image flagIMG = Toolkit.getDefaultToolkit().getImage("resources/FLAG.png")
+							.getScaledInstance(boton.getWidth() - 3, boton.getWidth() - 3, Image.SCALE_DEFAULT);
 					boton.setHorizontalAlignment(SwingConstants.LEFT);
-					
+
 					boton.setIcon(new ImageIcon(flagIMG));
-					
-				}else if (miTablero.getCasilla(coordenadaActual).isVelada()) {
+
+				} else if (miTablero.getCasilla(coordenadaActual).isVelada()) {
 					boton.setIcon(null);
 					boton.setHorizontalAlignment(SwingConstants.CENTER);
-				}else {
-					
+				} else {
+
 					boton.removeMouseListener(boton.getMouseListeners()[1]);
 					boton.addMouseListener(createMouseListener(false));
 					setClicked(boton);
 					// SI ESTA DESVELADA Y ES BOMBA
 					if (miTablero.getCasilla(coordenadaActual).isMina()) {
-						
-						Image img = Toolkit.getDefaultToolkit().getImage("resources/MINA.png").getScaledInstance(
-								boton.getWidth()-3, boton.getWidth()-3, Image.SCALE_DEFAULT);
+
+						Image img = Toolkit.getDefaultToolkit().getImage("resources/MINA.png")
+								.getScaledInstance(boton.getWidth() - 3, boton.getWidth() - 3, Image.SCALE_DEFAULT);
 						boton.setHorizontalAlignment(SwingConstants.LEFT);
 						boton.setIcon(new ImageIcon(img));
 						boton.setBackground(new Color(190, 61, 61));
-						
+
 						miGestion.getDificultad().getLongitud();
 
 						removeAllMouseListener();
 						mostrarDerrota();
 						reproducirEfectoDerrota();
-						terminado=true;
+						terminado = true;
 					} // SI ESTA DESVELADA Y NO ES BOMBA
 					else {
 						int minasAlrededor = miTablero.getCasilla(coordenadaActual).getMinasAlrededor();
@@ -162,16 +163,18 @@ public class ParaUI extends UI {
 		boton.removeMouseListener(null);
 		;
 	}
+
 	private void removeAllMouseListener() {
-        BotonCasilla[][] casillas = getPanelMinador().getBotones();
-        for(int i=0;i<casillas.length;i++) {
-            for(int j=0;j<casillas[0].length;j++) {
-            	if(casillas[i][j].getMouseListeners().length==2) {
-            		casillas[i][j].removeMouseListener(casillas[i][j].getMouseListeners()[1]);
-            	}
-            }
-        }
-    }
+		BotonCasilla[][] casillas = getPanelMinador().getBotones();
+		for (int i = 0; i < casillas.length; i++) {
+			for (int j = 0; j < casillas[0].length; j++) {
+				if (casillas[i][j].getMouseListeners().length == 2) {
+					casillas[i][j].removeMouseListener(casillas[i][j].getMouseListeners()[1]);
+				}
+			}
+		}
+	}
+
 	// COMPORTAMIENTO BOTON DIFICULTADES
 	public void behaviourDifficultyButtons(JMenuItem myButtons[]) {
 		// FACIL
@@ -180,7 +183,7 @@ public class ParaUI extends UI {
 				getPanelMinador().crearBotonera(Dificultad.facil);
 				miGestion = new GestionTablero(Dificultad.facil);
 				behaviourGameButtons();
-				
+
 				esconderPaneles();
 				// actualizar
 				getPanelMinador().revalidate();
@@ -193,7 +196,7 @@ public class ParaUI extends UI {
 				getPanelMinador().crearBotonera(Dificultad.medio);
 				miGestion = new GestionTablero(Dificultad.medio);
 				behaviourGameButtons();
-				
+
 				esconderPaneles();
 				// actualizar
 				getPanelMinador().revalidate();
@@ -206,7 +209,7 @@ public class ParaUI extends UI {
 				getPanelMinador().crearBotonera(Dificultad.dificil);
 				miGestion = new GestionTablero(Dificultad.dificil);
 				behaviourGameButtons();
-				
+
 				esconderPaneles();
 				// actualizar
 				getPanelMinador().revalidate();
@@ -226,9 +229,6 @@ public class ParaUI extends UI {
 		}
 	}
 
-	
-	
-	
 	public void mostrarVictoria() {
 		victoriaPanel.setVisible(true);
 	}
@@ -241,17 +241,18 @@ public class ParaUI extends UI {
 		gestionSonidos.reproducirEfectoDerrota();
 
 	}
+
 	private void reproducirEfectoVictoria() {
 		gestionSonidos.reproducirEfectoVictoria();
 	}
+
 	private void reprodicirEfecto() {
 		gestionSonidos.reproducirEfecto();
 	}
-	
+
 	private void esconderPaneles() {
 		this.derrotaPanel.setVisible(false);
 		this.victoriaPanel.setVisible(false);
 	}
-	
-	
+
 }
